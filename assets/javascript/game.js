e182bae356b28e4fd97dc1e7b62cc6a152227a92
MagicMayo <mike.mayoii@gmail.com>
2019-06-06 16:54:14 -0700

    added borders to images and made them fluid. added basic instruction and title for game. started second line for user selected character. added comments in js as an outline of what to do

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// Assign random attack to each character
+
+// After user selects character the rest are moved to the available to attack area
+
+// After user selects a character to attack it is moved to the defend area
+
+// Listener for when each character is defeated and restarts function for user to select a new character
+
+// Listener for when user character is defeated
+
+// Listener for attack button
+
+// Function for increasing attack each time attack button is pressed
