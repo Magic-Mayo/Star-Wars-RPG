2c6c9aacf2e702fbc3558a1254e935ae4c8c5ad3
Magic-Mayo <mike.mayoii@gmail.com>
2019-06-09 22:32:52 -0700

    finished functions to randomly assign hp and atk. changed id's to first letter capital on health spans and added character class to each character col in html.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,64 @@
 window.onload = function(){
-
+    getHP(4);
+    rndAtk(4);
 }
-// Assign random attack and health to each character
+
 let attack = [5, 10, 15, 20];
 let hp = [100, 200, 250, 150];
-let char = [$('#Yoda-health'), $('#Jinn-health'), $('#Chewy-health'), $('#Palpatine-health')]
-// let 
-function rndAtk(a){
-    for (let i = a.length - 1; i > 0; i--) {
-        const atkPwr = attack[Math.floor(Math.random() * attack.length)];
-    }
-}
+let atk = [];
+let health = [];
 
-function rndHP(count){
-
-    const tmp = hp.slice(hp);
-    let health = []
-
-    for (var i = 0; i < count; i++){
-        const a = Math.floor(Math.random() * tmp.length);
-        const rmv = tmp.splice(a, 1);
-        health.push(rmv[0]);
-    }
-
-    return health;
+// Assign random attack and health to each character
+function rndAtk(count){
+    const tmp = attack.slice(hp);
     
+    for (let i = 0; i < count; i++) {
+        const index = Math.floor(Math.random() * tmp.length);
+        const rmv = tmp.splice(index, 1);
+        atk.push(rmv[0]);
+    }
+
+    atk.forEach(rndAtk)
+    function rndAtk(attack, index){
+        $('.Yoda').attr('data-atk', atk[0]);
+        $('.Jinn').attr('data-atk', atk[1]);
+        $('.Chewy').attr('data-atk', atk[2]);
+        $('.Palpatine').attr('data-atk', atk[3]);
+    }
+    console.log($('.Yoda'))
 }
-rndHP();
-let health;
-$('#Yoda-health').text('HP: ' + rndHP(health[1]))
-$('#Jinn-health').text('HP: ' + rndHP([2]))
-$('#Chewy-health').text('HP: ' + rndHP([3]))
-$('#Palpatine-health').text('HP: ' + rndHP([1]))
-console.log(rndHP(4))
 
+function getHP(count){
+    const tmp = hp.slice(hp);
 
+    for (let i = 0; i < count; i++) {
+        const index = Math.floor(Math.random() * tmp.length);
+        const rmv = tmp.splice(index, 1);
+        health.push(rmv[0]);
+  }
 
-rndAtk(char);
-// hpAssign();
-// After user selects character the rest are moved to the available to attack area
+    health.forEach(rndHP)
+    function rndHP(hp, index){
+        $('#Yoda-health').text('HP: ' + health[0]).attr('data-hp', health[0]);
+        $('#Jinn-health').text('HP: ' + health[1]).attr('data-hp', health[1]);
+        $('#Chewy-health').text('HP: ' + health[2]).attr('data-hp', health[2]);
+        $('#Palpatine-health').text('HP: ' + health[3]).attr('data-hp', health[3]);
+    }
+    console.log($('#Yoda-health'))
+}
 
-// After user selects a character to attack it is moved to the defend area
-
-// Function for increasing attack each time attack button is pressed
+// After user selects character the rest are moved to the enemies to defeat area
+$(document).ready(function(){
+    $('.character').click(function(){
+        $('.character').appendTo($('#comp-char'));
+        console.log('hi');
+    });
+});
+// After user selects a character to attack it is moved to the attack arena area
 
 // Listener for attack button
+
+// Function for increasing attack each time attack button is pressed
 
 // Listener for when user character is defeated and restarts function for user to select a new character after a specified length of time
 
